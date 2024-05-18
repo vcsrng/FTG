affd0008b50293204b7684d6135494f4345245db
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-05-18 19:11:49 +0700

    Inventory thumbnail

---
+++ FTG/View/ModelThumbnailView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelThumbnailView.swift
+//  FTG
+//
+//  Created by Vincent Saranang on 18/05/24.
+//
+
+import SwiftUI
+
+struct ModelThumbnailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelThumbnailView()
+}
---
+++ FTG/View/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  FTG
+//
+//  Created by Vincent Saranang on 18/05/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
