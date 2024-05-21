cbd87189cd73813c6d704872ff6029e4d3dd246a
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-05-22 00:48:21 +0700

    GameGuide

---
+++ FTG/View/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  FTG
+//
+//  Created by Vincent Saranang on 21/05/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
